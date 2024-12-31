d516a6b78af9609ff40b5a2b27b211845e7a3d4e
Subhadip Das <subhadipdas911@gmail.com>
2024-12-31 22:35:01 +0530

    Updated the code, now @Test does not contains synchronization code it only involves Validation. Fine tuned, login situation, now with unsuccessful login Test case will fail and not test case execution will stop immediately

--- src/test/java/testCases/TC003_ValidUserLoginTest.java
+++ src/test/java/testCases/TC003_ValidUserLoginTest.java
@@ -3,59 +3,72 @@ package testCases;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import pageObjects.AccountLogoutPage;
 import pageObjects.HomePage;
 import pageObjects.LoginPage;
 import pageObjects.MyAccountPage;
 import testBase.BaseTest;
 
 public class TC003_ValidUserLoginTest extends BaseTest {
-	
-	@Test(groups= {"sanity","master"})
-	public void validate_valid_user_credentials() {
-		
-		logger.info("**** Starting TC003_ValidUserLoginTest execution ****");
-		
-		try {
-		HomePage homePage = new HomePage(driver);
-		homePage.waitForTitleToLoad("Your Store");
-		logger.info("Inside Home Page");
-		homePage.clickMyAccount();
-		logger.info("Clicked My Account");
-		homePage.clickLogin();
-		logger.info("Clicked Login Link");
-		
-		LoginPage loginPage = new LoginPage(driver);
-		loginPage.waitForTitleToLoad("Account Login");
-		logger.info("Inside Login Page");
-		logger.info("Entering user credentials");
-		
-		loginPage.enterUserEmail(properties.getProperty("email"));
-		loginPage.enterUserPassword(properties.getProperty("password"));
-		loginPage.clickLoginButton();
-		
-		logger.info("Clicked on Continue button");
 
-//		Validation Part :-
-		
-		MyAccountPage myAccount = new MyAccountPage(driver);
-		myAccount.waitForTitleToLoad("My Account");
-		
-		logger.info("Starting validation");
-		
-		boolean confirmMessage = myAccount.getHeaderMessage();
-		
-		if (confirmMessage) {
-			logger.info("Validation successfull");
+	@Test(groups = { "sanity", "master" })
+	public void validate_valid_user_credentials() {
+
+		logger.info("**** Starting TC003_ValidUserLoginTest execution ****");
+
+		try {
+			HomePage homePage = new HomePage(driver);
+			boolean homePageStatus = homePage.checkPageStatus(properties.getProperty("homePageTitle"));
+
+			if (!homePageStatus) {
+				logger.info("Could not load Home Page...");
+				Assert.fail();
+			}
+
+			logger.info("Inside Home Page");
+			homePage.clickMyAccount();
+			logger.info("Clicked My Account");
+			homePage.clickLogin();
+			logger.info("Clicked Login Link");
+
+			LoginPage loginPage = new LoginPage(driver);
+			boolean loginPageStatus = loginPage.checkPageStatus(properties.getProperty("loginPageTitle"));
+
+			if (!loginPageStatus) {
+				logger.info("Could not load Login Page ....");
+				Assert.fail();
+			}
+
+			logger.info("Inside Login Page");
+			logger.info("Entering user credentials");
+			loginPage.enterUserEmail(properties.getProperty("email"));
+			loginPage.enterUserPassword(properties.getProperty("password"));
+			loginPage.clickLoginButton();
+
+			logger.info("Clicked on Continue button");
+
+			MyAccountPage myAccount = new MyAccountPage(driver);
+			boolean myAccountStatus = myAccount.checkPageStatus(properties.getProperty("myAccountPageTitle"));
+			
+			if (!myAccountStatus) {
+				logger.info("Invalid Credentials.");
+				Assert.fail();
+			}
+			
+			logger.info("Successful login !");
+
+			myAccount.clickLogout();
+			
+			AccountLogoutPage logoutPage = new AccountLogoutPage(driver);
+			logoutPage.clickContinueButton();
+			
 			Assert.assertTrue(true);
-		}else {
-			logger.error("Validation failure");
-			Assert.assertTrue(false);
-		}
-		} catch(Exception e) {
-			logger.error("Error Message -> "+e.getMessage());
+			
+		} catch (Exception e) {
+			logger.error("Error Message -> " + e.getMessage());
 			Assert.fail();
 		}
-		
+
 		logger.info("**** Finished TC003_ValidUserLoginTest execution ****");
 	}
 }
