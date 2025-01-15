ac1d55e8619b45cfb683c17f9c3dc3b0341dd604
Subhadip Das <subhadipdas911@gmail.com>
2025-01-15 21:05:06 +0530

    Reframed the Test Case to handle Parallel Execution. Significant changes made towards using the driver instance and fetching data from Properties object

--- src/test/java/testCases/TC003_ValidUserLoginTest.java
+++ src/test/java/testCases/TC003_ValidUserLoginTest.java
@@ -17,8 +17,8 @@ public class TC003_ValidUserLoginTest extends BaseTest {
 		logger.info("**** Starting TC003_ValidUserLoginTest execution ****");
 
 		try {
-			HomePage homePage = new HomePage(driver);
-			boolean homePageStatus = homePage.checkPageStatus(properties.getProperty("homePageTitle"));
+			HomePage homePage = new HomePage(tLocalDriver.get());
+			boolean homePageStatus = homePage.checkPageStatus(tLocalProperties.get().getProperty("homePageTitle"));
 
 			if (!homePageStatus) {
 				logger.info("Could not load Home Page...");
@@ -31,8 +31,8 @@ public class TC003_ValidUserLoginTest extends BaseTest {
 			homePage.clickLogin();
 			logger.info("Clicked Login Link");
 
-			LoginPage loginPage = new LoginPage(driver);
-			boolean loginPageStatus = loginPage.checkPageStatus(properties.getProperty("loginPageTitle"));
+			LoginPage loginPage = new LoginPage(tLocalDriver.get());
+			boolean loginPageStatus = loginPage.checkPageStatus(tLocalProperties.get().getProperty("loginPageTitle"));
 
 			if (!loginPageStatus) {
 				logger.info("Could not load Login Page ....");
@@ -41,29 +41,30 @@ public class TC003_ValidUserLoginTest extends BaseTest {
 
 			logger.info("Inside Login Page");
 			logger.info("Entering user credentials");
-			loginPage.enterUserEmail(properties.getProperty("email"));
-			loginPage.enterUserPassword(properties.getProperty("password"));
+			loginPage.enterUserEmail(tLocalProperties.get().getProperty("email"));
+			loginPage.enterUserPassword(tLocalProperties.get().getProperty("password"));
 			loginPage.clickLoginButton();
 
 			logger.info("Clicked on Continue button");
 
-			MyAccountPage myAccount = new MyAccountPage(driver);
-			boolean myAccountStatus = myAccount.checkPageStatus(properties.getProperty("myAccountPageTitle"));
-			
+			MyAccountPage myAccount = new MyAccountPage(tLocalDriver.get());
+			boolean myAccountStatus = myAccount
+					.checkPageStatus(tLocalProperties.get().getProperty("myAccountPageTitle"));
+
 			if (!myAccountStatus) {
 				logger.info("Invalid Credentials.");
 				Assert.fail();
 			}
-			
+
 			logger.info("Successful login !");
 
 			myAccount.clickLogout();
-			
-			AccountLogoutPage logoutPage = new AccountLogoutPage(driver);
+
+			AccountLogoutPage logoutPage = new AccountLogoutPage(tLocalDriver.get());
 			logoutPage.clickContinueButton();
-			
+
 			Assert.assertTrue(true);
-			
+
 		} catch (Exception e) {
 			logger.error("Error Message -> " + e.getMessage());
 			Assert.fail();
